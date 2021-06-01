724d907f011531bdb280c923a29bd5e142555a90
Tom Holohan <thomasholohan8@gmail.com>
2021-06-01 12:37:42 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Connected!")
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractionQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
