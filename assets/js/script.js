d6d01bf9fd93746163495148a9354bc1a8171a5d
Tom Holohan <thomasholohan8@gmail.com>
2021-06-02 12:12:57 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType){
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2)
+    } else if (gameType === "subtract"){
+        displaySubtractionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -75,11 +77,12 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition"];
     } else if (operator === "x"){
         return [operand1 * operand2, "multiply"]
-    } else {
+    } else if (operator === "-"){
+        return [operand1 - operand2, "subtract"]
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     } 
-
 }
 
 
@@ -97,7 +100,6 @@ function incrementScore(){
  * Gets the current tally of incorrect answers from the DOM and increments it by 1
  */
 function incrementWrongAnswer(){
-
     let oldScore = parseInt(document.getElementById('incorrect').innerText);
     document.getElementById('incorrect').innerText = ++oldScore;
 }
@@ -108,14 +110,14 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractionQuestion(){
-    
+function displaySubtractionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2){
-    
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
-
 }
